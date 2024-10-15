7e53495f3ff47e5f1ef131b55a606d973a05469f
Iancovski <46848500+Iancovski@users.noreply.github.com>
2024-10-14 22:42:29 -0300

    Main Page
    Done main page layout

--- src/app/pages/main/main.component.ts
+++ src/app/pages/main/main.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { GradientButtonComponent } from "../../components/gradient-button/gradient-button.component";
 
 @Component({
     selector: 'main-page',
     standalone: true,
-    imports: [],
+    imports: [GradientButtonComponent],
     templateUrl: './main.component.html',
     styleUrl: './main.component.scss'
 })
