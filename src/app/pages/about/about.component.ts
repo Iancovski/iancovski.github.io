804762ebef9d219a8cdf12ca64681b6708c438f0
Iancovski <46848500+Iancovski@users.noreply.github.com>
2024-10-15 23:28:36 -0300

    About Page
    Done about page paragraph

--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -1,12 +1,13 @@
+import { NgClass } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
     selector: 'about-page',
     standalone: true,
-    imports: [],
+    imports: [NgClass],
     templateUrl: './about.component.html',
     styleUrl: './about.component.scss'
 })
 export class AboutComponent {
-
+    protected expanded: boolean = false;
 }
