52329b1201da0ed2444f30fa22a162c04c3d6b46
Iancovski <46848500+Iancovski@users.noreply.github.com>
2024-10-23 19:54:30 -0300

    General layout adjustments

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,11 +5,12 @@ import { AboutComponent } from "./pages/about/about.component";
 import { ExperienceComponent } from "./pages/experience/experience.component";
 import { ProjectsComponent } from "./pages/projects/projects.component";
 import { ContactComponent } from "./pages/contact/contact.component";
+import { SeparatorComponent } from "./components/separator/separator.component";
 
 @Component({
     selector: 'app-root',
     standalone: true,
-    imports: [RouterOutlet, MainComponent, AboutComponent, ExperienceComponent, ProjectsComponent, ContactComponent],
+    imports: [RouterOutlet, MainComponent, AboutComponent, ExperienceComponent, ProjectsComponent, ContactComponent, SeparatorComponent],
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss'
 })
---
+++ src/app/components/separator/separator.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'separator',
+    standalone: true,
+    imports: [],
+    templateUrl: './separator.component.html',
+    styleUrl: './separator.component.scss'
+})
+export class SeparatorComponent {
+
+}
