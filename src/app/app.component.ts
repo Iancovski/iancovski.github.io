f4acb482d245217fcb4b8ce5f57c5b0bca1184d0
Iancovski <46848500+Iancovski@users.noreply.github.com>
2024-10-28 22:28:52 -0300

    Menu
    Added hamburger menu with overlay

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,14 +6,41 @@ import { ExperienceComponent } from "./pages/experience/experience.component";
 import { ProjectsComponent } from "./pages/projects/projects.component";
 import { ContactComponent } from "./pages/contact/contact.component";
 import { SeparatorComponent } from "./components/separator/separator.component";
+import { NgClass } from '@angular/common';
 
 @Component({
     selector: 'app-root',
     standalone: true,
-    imports: [RouterOutlet, MainComponent, AboutComponent, ExperienceComponent, ProjectsComponent, ContactComponent, SeparatorComponent],
+    imports: [RouterOutlet, MainComponent, AboutComponent, ExperienceComponent, ProjectsComponent, ContactComponent, SeparatorComponent, NgClass],
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss'
 })
 export class AppComponent {
     title = 'landing-page';
+
+    isMenuOpen = false;
+
+    toggleMenu() {
+        this.isMenuOpen = !this.isMenuOpen;
+
+        if (this.isMenuOpen) {
+            document.body.classList.add('disable-scroll');
+        } else {
+            document.body.classList.remove('disable-scroll');
+        }
+    }
+
+    closeMenu() {
+        this.isMenuOpen = false;
+        document.body.classList.remove('disable-scroll');
+    }
+
+    scrollTo(id: string) {
+        const element = document.getElementById(id);
+
+        if (element) {
+            element.scrollIntoView({ behavior: 'smooth', block: 'start' });
+            this.isMenuOpen = false;
+        }
+    }
 }
