5b506ae4de55511882c8ad55d61edafb12baa8e0
Iancovski <46848500+Iancovski@users.noreply.github.com>
2024-11-09 23:44:31 -0300

    Form Field Component
    Created custom input

---
+++ src/app/components/form-field/form-field.component.ts
@@ -0,0 +1,15 @@
+import { NgClass } from '@angular/common';
+import { Component, Input } from '@angular/core';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
+
+@Component({
+    selector: 'form-field',
+    standalone: true,
+    imports: [ReactiveFormsModule, NgClass],
+    templateUrl: './form-field.component.html',
+    styleUrl: './form-field.component.scss'
+})
+export class FormFieldComponent {
+    @Input() label: string = '';
+    @Input() control!: FormControl;
+}
