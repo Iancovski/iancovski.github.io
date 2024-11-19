f196c97c64b479bc796b2c4ed7bf1f56e3461b8f
Iancovski <46848500+Iancovski@users.noreply.github.com>
2024-11-18 21:33:14 -0300

    Contact Page
    Done contact page layout

--- src/app/pages/contact/contact.component.ts
+++ src/app/pages/contact/contact.component.ts
@@ -1,12 +1,24 @@
+import { NgOptimizedImage } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormFieldComponent } from "../../components/form-field/form-field.component";
+import { GradientButtonComponent } from "../../components/gradient-button/gradient-button.component";
 
 @Component({
     selector: 'contact-page',
     standalone: true,
-    imports: [],
+    imports: [NgOptimizedImage, ReactiveFormsModule, FormFieldComponent, GradientButtonComponent],
     templateUrl: './contact.component.html',
     styleUrl: './contact.component.scss'
 })
 export class ContactComponent {
+    form = new FormGroup({
+        name: new FormControl('', Validators.required),
+        email: new FormControl('', Validators.required),
+        subject: new FormControl('', Validators.required),
+        message: new FormControl('', Validators.required)
+    });
 
+    sendMessage() {
+    }
 }
